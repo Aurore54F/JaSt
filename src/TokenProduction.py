93b44baf6c5eff8365775623ac198460afdee49b
Aurore54F <aurore.fass.pro@gmail.com>
2017-04-11 15:13:21 +0200

    TokenProduction for esprima

--- src/TokenProduction.py
+++ src/TokenProduction.py
@@ -5,33 +5,60 @@
 
 from slimit.lexer import Lexer
 
-# SlimIt
-def buildToken(inputFile,outputFile):
+import subprocess # to call Shell commands
+
+
+def buildToken(parser,inputFile,outputFile):
 	'''
 		Given an input JavaScript file, write the list of tokens used in an output file.
 	'''
 	
-	inF = open(inputFile,'r');
-	s = '';
-	for line in inF:
-		s += str(line); # Store the content of the JS file in a string
-	inF.close();
+	if parser.lower() == 'slimit':
 	
-	lexer = Lexer();
-	lexer.input(s);
-
-	outF = open(outputFile,'w');
-
-	for token in lexer:
-		value = token;
-		s = str(value); # Conversion of token type to string
-		# Structure of a token: "LexToken(VAR,'var',1,0)"
-		tokenPart = s.split('(');
-		tokenComplete = tokenPart[1].split(','); # Keyword as used in JS
+		inF = open(inputFile,'r');
+		s = '';
+		for line in inF:
+			s += str(line); # Store the content of the JS file in a string
+		inF.close();
 		
-		outF.write(tokenComplete[0] + '\n');
+		lexer = Lexer();
+		lexer.input(s);
+
+		outF = open(outputFile,'w');
+
+		for token in lexer:
+			value = token;
+			s = str(value); # Conversion of token type to string
+			# Structure of a token: "LexToken(VAR,'var',1,0)"
+			tokenPart = s.split('(');
+			tokenComplete = tokenPart[1].split(','); # Keyword as used in JS
+			
+			outF.write(tokenComplete[0] + '\n');
+			
+		outF.close();
 		
-	outF.close();
+		
+	elif parser.lower() == 'esprima':
+		
+		tempFile = 'Tempo.txt';
+		subprocess.call("slimit --mangle < " + inputFile + " > " + tempFile, shell = True); # Minify the input file
+		
+		tFile = open(tempFile,'r');
+		s = "var esprima = require('esprima');\nesprima.tokenize('";
+		for line in tFile:
+			s += line;
+		s += "', {}, function (node) {\n\tconsole.log(node.type);\n});"
+		#return s;
+		tFile.close();
+		tFile = open(tempFile,'w');
+		
+		tFile.write(s);
+		tFile.close();
+		
+		subprocess.call("node " + tempFile + " > " + outputFile, shell = True); # Esprima
+		
+	else:
+		print("Error on the parser's name. Indicate 'slimIt' or 'esprima'.");
 	
 	
 	
