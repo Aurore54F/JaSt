b320ea2593c7cc39b1e54e0055b4a74213c2babf
Aurore54F <aurore.fass.pro@gmail.com>
2017-05-10 07:22:51 +0200

    Correction for Js detection

--- JsDetection/JsDetection.py
+++ JsDetection/JsDetection.py
@@ -16,7 +16,7 @@ def isJs(jsDir, parser = 'esprimaAst'):
 	'''
 	#subprocess.run(['node' , '../src/JsEsprima/parser.js', inputFile], stdout = subprocess.PIPE).stdout.decode('utf-8');
 	#subprocess.run(['node' , '../src/JsEsprima/tokenizer.js', inputFile], stdout = subprocess.PIPE).stdout.decode('utf-8');
-	#subprocess.run(['slimit', '--mangle', inputFile], stdout = subprocess.PIPE).stdout.decode('utf-8');
+	#subprocess.run(['node' , '../src/JsEsprima/parser.js', givenFile], stdout = subprocess.PIPE).stdout.decode('utf-8');
 
 	if not os.path.exists('NotJs'):
 		os.makedirs('NotJs');
@@ -26,43 +26,43 @@ def isJs(jsDir, parser = 'esprimaAst'):
 	if parser.lower() == 'slimit':
 		for givenFile in sorted(l):
 			#result = subprocess.run('slimit --mangle ' + givenFile + ' 2> /dev/null', stdout = subprocess.PIPE).stdout.decode('utf-8');
-			if result == '':
-				print('The file ' + givenFile + ' is not considered as a valid JavaScript file.');
-				
-			'''
-				try:
-					subprocess.check_output('slimit --mangle ' + givenFile + ' 2> /dev/null', shell = True)
-				except subprocess.CalledProcessError as e:
-					if  e.returncode == 1:
-						#subprocess.run('mv ' + givenFile + ' NotJs');
+			try:
+				subprocess.check_output('slimit --mangle ' + givenFile + ' 2> /dev/null', shell = True)
+			except subprocess.CalledProcessError as e:
+				if  e.returncode == 1:
+					#subprocess.run('mv ' + givenFile + ' NotJs');
+					print('Return: ' + str(e.output) + '\n');
+					if str(e.output) == "b''":
 						print('The file ' + givenFile + ' is not considered as a valid JavaScript file.');
-			'''
+					else:
+						print('Error on the file ' + givenFile + ' structure.');
 				
 	elif parser.lower() == 'esprima':
 		for givenFile in sorted(l):
-			'''
-				try:
-					subprocess.check_output('node ../src/JsEsprima/tokenizer.js ' + givenFile + ' 2> /dev/null', shell = True)
-				except subprocess.CalledProcessError as e:
-					if  e.returncode == 1:
-						#subprocess.run('mv ' + givenFile + ' NotJs');
+			try:
+				subprocess.check_output('node ../src/JsEsprima/tokenizer.js ' + givenFile + ' 2> /dev/null', shell = True)
+			except subprocess.CalledProcessError as e:
+				if  e.returncode == 1:
+					#subprocess.run('mv ' + givenFile + ' NotJs');
+					print('Return: ' + str(e.output) + '\n');
+					if str(e.output) == "b''":
 						print('The file ' + givenFile + ' is not considered as a valid JavaScript file.');
-			'''
+					else:
+						print('Error on the file ' + givenFile + ' structure.');
 				
 	elif parser.lower() == 'esprimaast':
 		for givenFile in sorted(l):
-			#result = subprocess.run(['node' , '../src/JsEsprima/parser.js', givenFile], stdout = subprocess.PIPE).stdout.decode('utf-8');
-			#if result == '':
-				#print('The file ' + givenFile + ' is not considered as a valid JavaScript file.');
-
 			try:
 				subprocess.check_output('node ../src/JsEsprima/parser.js ' + givenFile + ' 2> /dev/null', shell = True)
 			except subprocess.CalledProcessError as e:
 				if  e.returncode == 1:
 					#subprocess.run('mv ' + givenFile + ' NotJs');
-					print(str(e.output) + '\n');
-					print('The file ' + givenFile + ' is not considered as a valid JavaScript file.');
-			
+					print('Return: ' + str(e.output) + '\n');
+					if str(e.output) == "b''":
+						print('The file ' + givenFile + ' is not considered as a valid JavaScript file.');
+					else:
+						print('Error on the file ' + givenFile + ' structure.');
+						
 	else:
 		print("Error on the parser's name. Indicate 'slimIt', 'esprima' or 'esprimaAst'.");
 		return;
