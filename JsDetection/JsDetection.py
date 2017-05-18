7a90d0c684a652b3c551e38a10850dbe0dde8c08
Aurore54F <aurore.fass.pro@gmail.com>
2017-05-18 14:24:54 +0200

    JsDetection through the command-line

--- JsDetection/JsDetection.py
+++ JsDetection/JsDetection.py
@@ -7,7 +7,6 @@
 
 import subprocess # to call Shell commands
 import glob # Unix style pathname pattern expansion
-import sys
 import argparse
 
 notJs = [];
@@ -17,20 +16,22 @@ structureError = [];
 
 parser = argparse.ArgumentParser(description='Given a repository containing .bin or .js files, indicate whether they are (valid) JavaScript files or not.'); # Creating an ArgumentParser object
 # The ArgumentParser object holds all the information necessary to parse the command line into Python data types.
-#parser.add_argument(metavar = 'Dir', type=str, help='list of files');
-parser.add_argument('Dir', nargs='+', help='list of files');
-#parser.add_argument('--f', help='list of files');
-#parser.add_argument('--summary', help='print a summary of the state of the files');
+
+#parser.add_argument('Dir', help='list of files');
+
+parser.add_argument('dir', nargs='*', help='directory containing files to be tested');
+parser.add_argument('--f', nargs='+', help='list of files to be tested');
+parser.add_argument('--s', help='print a summary of the state of the files');
 	
-args = parser.parse_args();
+args = vars(parser.parse_args());
 
-'''
-	Given a repository containing .bin or .js files, indicate whether they are (valid) JavaScript files or not.
-'''
 
-l = glob.glob(sys.argv[1] + '/*.js') + glob.glob(sys.argv[1] + '/*.bin'); # Extension in .bin or .js
-			
-for givenFile in sorted(l):
+def isJsFile(givenFile):
+	global notJs;
+	global structureError;
+	notJs = [];
+	structureError = [];
+
 	try:
 		subprocess.check_output('node ../src/JsEsprima/parser.js ' + givenFile + ' 2> /dev/null', shell = True)
 	except subprocess.CalledProcessError as e:  # TODO catch exception if file cannot be opened
@@ -38,15 +39,37 @@ for givenFile in sorted(l):
 			if str(e.output) == "b''": # The file could not be parsed: not a JS sample
 				print('File ' + givenFile + ': not JavaScript');
 				notJs += [givenFile];
+				return;
 			else: # The file could partially be parsed: malformed JS
 				print('File ' + givenFile + ': invalid JavaScript');
 				structureError += [givenFile];
+				return;
+	print('File ' + givenFile + ': valid JavaScript');
 	
-print('\n\n Summary:\n');
-print('* The following files are not considered as JavaScript:');
-for el in notJs:
-	print('\t- ' + el);
 
-print('\n* The following files structure is not correct:\n');
-for el in structureError:
-	print('\t- ' + el);
+'''
+	Given a repository containing .bin or .js files, indicate whether they are (valid) JavaScript files or not.
+'''
+
+if args['dir'] != []:
+	for el in args['dir']:
+		l = glob.glob(el + '/*.js') + glob.glob(el + '/*.bin'); # Extension in .bin or .js			
+		for givenFile in sorted(l):
+			isJsFile(givenFile);
+
+
+if args['f'] != None:
+	for givenFile in args['f']:
+		isJsFile(givenFile);
+
+
+if args['s'] != None:
+	
+	print('\n\n Summary:\n');
+	print('* The following files are not considered as JavaScript:');
+	for el in notJs:
+		print('\t- ' + el);
+	
+	print('\n* The following files structure is not correct:\n');
+	for el in structureError:
+		print('\t- ' + el);
