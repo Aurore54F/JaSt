bf7b5e27bf569731175f4261a7933f925a3ec03d
Aurore54F <aurore.fass.pro@gmail.com>
2017-09-28 11:40:09 +0200

    Added Esprima

--- install.sh
+++ install.sh
@@ -20,8 +20,8 @@ pip3 install pandas # (0.20.3)
 
 sudo apt install nodejs # version 4 or later including its package manager npm (4.2.6)
 sudo apt-get install nodejs-legacy # (4.2.6)
-sudo apt install npm # (3.5.2) Did not work on Debian testing
-npm install esprima # (3.1.3) or (4.0.0)
+#sudo apt install npm # (3.5.2) Did not work on Debian testing
+#npm install esprima # (3.1.3) or (4.0.0)
 
 
 # Machine Learning workbenches
