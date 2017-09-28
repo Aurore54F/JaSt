4f451a27309f91eaf5a42404a09414ee0213a53a
Aurore54F <aurore.fass.pro@gmail.com>
2017-09-28 13:47:14 +0200

    Ran Pylint

--- MachineLearning/Classification.py
+++ MachineLearning/Classification.py
@@ -4,19 +4,17 @@
 '''
 
 import os
-import argparse # To parse command line arguments
+import pickle
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix
-from sklearn import metrics
-from sklearn.metrics import roc_curve, auc
-import pickle
+from sklearn.metrics import confusion_matrix, roc_curve, auc
+from sklearn.model_selection import StratifiedKFold
 
 from scipy import interp
-from sklearn.model_selection import StratifiedKFold
+
 
 currentPath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 from __init__ import *
@@ -26,7 +24,7 @@ def classify(trainingFile, modelDir=currentPath+'/Classification/',\
              modelName='/model', printScore=False, printRes=False):
     '''
         Training a Naive Bayes Multinomial classifier.
-      
+
         -------
         Parameter:
         - trainingFile: String
@@ -64,39 +62,44 @@ def classify(trainingFile, modelDir=currentPath+'/Classification/',\
     data = pd.read_csv(trainingFile)
 
     names = data['Outlook'] # Vector containing all the names of the samples considered
-    labels = data['Label'] # Vector containing all the labels (i.e. 'benign', 'malicious', or '?') of the samples considered
-    X = data.ix[:, '0':] # # 2D vector containing all the attributes of the samples considered
+    labels = data['Label'] # Vector containing all the labels (i.e. 'benign', 'malicious',
+        #or '?') of the samples considered
+    X = data.ix[:, '0':] # 2D vector containing all the attributes of the samples considered
     X = np.asarray(X)
 
     clf = MultinomialNB()
     trained = clf.fit(X, labels) # Model
 
-    clf.predict_proba(X) # Returns the probability of the samples for each class in the model. The columns correspond 
-        #to the classes in sorted order, as they appear in the attribute classes
+    clf.predict_proba(X) # Returns the probability of the samples for each class in the model.
+        #The columns correspond to the classes in sorted order, as they appear in the
+        #attribute classes
 
-    labelsPredicted = clf.predict(X) # Perform classification on an array of test vectors X and predict the target values
-    accuracy = clf.score(X,labels) # Detection rate
-    
+    labelsPredicted = clf.predict(X) # Perform classification on an array of test vectors X and
+        #predict the target values
+    accuracy = clf.score(X, labels) # Detection rate
+
     if printScore:
-        TN, FP, FN, TP = confusion_matrix(labels, labelsPredicted).ravel() # y = labels, predicted = labels predicted
+        TN, FP, FN, TP = confusion_matrix(labels, labelsPredicted).ravel()
+        # y = labels, predicted = labels predicted
         print("Detection: " + str(accuracy))
         print("TP: " + str(TP) + ", FP: " + str(FP) + ", FN: " + str(FN) + ", TN: " + str(TN))
-    
+
     if printRes:
         for i in range(0, len(names)):
             print(str(names[i]) + ': ' + str(labelsPredicted[i]) + ' (' + str(labels[i]) + ')')
         print('> Name: labelPredicted (trueLabel)')
-    
+
     pickle.dump(trained, open(modelDir + modelName, 'wb'))
     print('The model has been successfully stored in ' + modelDir + modelName)
-    
-    return trained;
+
+    return trained
 
 
 def validate(validationFile, model, modelDir=currentPath+'/Classification/',\
              modelName='/model'):
     '''
-        Extension of a Naive Bayes Multinomial classification model with a new CSV file of the same format.
+        Extension of a Naive Bayes Multinomial classification model with a new CSV file of
+        the same format.
 
         -------
         Parameter:
@@ -104,7 +107,8 @@ def validate(validationFile, model, modelDir=currentPath+'/Classification/',\
             Path of the CSV file to be used to extend the following model.
         - model
             Naive Bayes Multinomial model.
-        Beware: the model must have been constructed using files of the same format (i.e. same attributes) as the format of validationFile.
+        Beware: the model must have been constructed using files of the same format
+        (i.e. same attributes) as the format of validationFile.
 
         -------
         Returns:
@@ -121,17 +125,19 @@ def validate(validationFile, model, modelDir=currentPath+'/Classification/',\
     XValidation = validationSet.ix[:, '0':] # Split off features
     XValidation = np.asarray(XValidation)
 
-    validated = model.partial_fit(XValidation, labelsValidation, ['malicious', 'benign']) # Incremental fit on a batch of samples
+    validated = model.partial_fit(XValidation, labelsValidation, ['malicious', 'benign'])
+    # Incremental fit on a batch of samples
 
     pickle.dump(validated, open(modelDir + modelName, 'wb'))
     print('The model has been successfully stored in ' + modelDir + modelName)
-    
+
     return validated
 
 
 def testModel(testFile, model, printRes=True):
     '''
-        Test of a Naive Bayes Multinomial classification model with a new CSV file of the same format.
+        Test of a Naive Bayes Multinomial classification model with a new CSV file of the
+        same format.
 
         -------
         Parameter:
@@ -139,13 +145,15 @@ def testModel(testFile, model, printRes=True):
             Path of the CSV file to be tested using the following model.
         - model
             Naive Bayes Multinomial model.
-        Beware: the model must have been constructed using files of the same format (i.e. same attributes) as the format of testFile.
+        Beware: the model must have been constructed using files of the same format
+        (i.e. same attributes) as the format of testFile.
 
         -------
         Returns:
         - Naive Bayes Multinomial model.
-            Beware: the model was implemented as a global variable in sklearn TODO.
-        - Print the detection rate and the TP, FP, FN and TN rates of testFile tested with model, in stdout.
+            Beware: the model was implemented as a global variable in sklear.
+        - Print the detection rate and the TP, FP, FN and TN rates of testFile
+        tested with model, in stdout.
     '''
 
     if type(model) is str:
@@ -158,22 +166,20 @@ def testModel(testFile, model, printRes=True):
     XTest = testSet.ix[:, '0':] # Split off features
     XTest = np.asarray(XTest)
 
-    labelsPredictedTest = model.predict(XTest) # Perform classification on an array of test vectors X and predict the target values
-    labelsPredictedProbaTest = model.predict_proba(XTest) # Perform classification on an array of test vectors X and predict the target values
+    labelsPredictedTest = model.predict(XTest) # Perform classification on an array of test
+        #vectors X and predict the target values
+    labelsPredictedProbaTest = model.predict_proba(XTest) # Perform classification on an array of
+        #test vectors X and predict the target values
     accuracyTest = model.score(XTest, labelsTest) # Detection rate
 
-    #TN_test, FP_test, FN_test, TP_test = confusion_matrix(labelsTest, labelsPredictedTest).ravel() # y = labels, predicted = labels predicted
-
-    #print("Detection: " + str(accuracyTest))
-    #print("TN: " + str(TN_test) + ", FP: " + str(FP_test) + ", TP: " + str(TP_test) + ", FN: " + str(FN_test))
-    
     if printRes:
         for i in range(0, len(names)):
-            print(str(names[i]) + ': ' + str(labelsPredictedTest[i]) + ' (' + str(labelsTest[i]) + ')')
+            print(str(names[i]) + ': ' + str(labelsPredictedTest[i]) + ' ('\
+                  + str(labelsTest[i]) + ')')
         print('> Name: labelPredicted (trueLabel)')
 
     '''
-    fpr, tpr, thresholds = roc_curve(labelsTest, labelsPredictedProbaTest[:,0],pos_label = 'benign')
+    fpr, tpr, thresholds = roc_curve(labelsTest, labelsPredictedProbaTest[:, 0], pos_label='benign')
     print('Threshold: ' + str(thresholds))
     print("AUC of the predictions: {0}".format(metrics.auc(fpr, tpr)))
     roc_auc = auc(fpr,tpr)
@@ -195,14 +201,15 @@ def testModel(testFile, model, printRes=True):
 
 
 
-def crossValidation(trainingFile, n_splits = 10):
+def crossValidation(trainingFile, n_splits=10):
     '''
         x-fold cross-validation and ROC analysis.
     '''
 
     data = pd.read_csv(trainingFile)
 
-    labels = data['Label'] # Vector containing all the labels (i.e. 'benign', 'malicious', or '?') of the samples considered
+    labels = data['Label'] # Vector containing all the labels (i.e. 'benign', 'malicious', or '?')
+        #of the samples considered
     X = data.ix[:, '0':] # 2D vector containing all the attributes of the samples considered
     X = np.asarray(X)
 
@@ -214,24 +221,28 @@ def crossValidation(trainingFile, n_splits = 10):
     all_tpr = []
 
     i = 0
-    for train, test in cv.split(X,labels):
+    for train, test in cv.split(X, labels):
         model = classifier.fit(X[train], labels[train])
         labelsPredicted = model.predict(X[test])
         labelsPredictedProba = model.predict_proba(X[test])
-        # Compute ROC curve and area the curve      
+        # Compute ROC curve and area the curve
 
         accuracyTest = model.score(X, labels) # Detection rate
 
-        TN_test, FP_test, FN_test, TP_test = confusion_matrix(labels[test], labelsPredicted).ravel() # y = labels, predicted = labels predicted
+        TN_test, FP_test, FN_test, TP_test = confusion_matrix(labels[test], labelsPredicted).ravel()
+        # y = labels, predicted = labels predicted
 
         print("Detection: " + str(accuracyTest))
-        print("TN: " + str(TN_test) + ", FP: " + str(FP_test) + ", TP: " + str(TP_test) + ", FN: " + str(FN_test))
+        print("TN: " + str(TN_test) + ", FP: " + str(FP_test) + ", TP: " + str(TP_test) + ", FN: "\
+              + str(FN_test))
 
-        fpr, tpr, thresholds = roc_curve(labels[test], labelsPredictedProba[:,0],pos_label = 'benign')
+        fpr, tpr, thresholds = roc_curve(labels[test], labelsPredictedProba[:, 0],\
+                                         pos_label='benign')
         mean_tpr += interp(mean_fpr, fpr, tpr)
         mean_tpr[0] = 0.0
         roc_auc = auc(fpr, tpr)
-        plt.plot(fpr, tpr, lw=1, label='ROC fold ' + str(i) + ' (AUC = ' + str(round(roc_auc,4)) + ')')
+        plt.plot(fpr, tpr, lw=1, label='ROC fold ' + str(i) + ' (AUC = ' + str(round(roc_auc, 4))\
+                 + ')')
         i += 1
 
     plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Luck')
@@ -240,7 +251,7 @@ def crossValidation(trainingFile, n_splits = 10):
     mean_tpr[-1] = 1.0
     mean_auc = auc(mean_fpr, mean_tpr)
     plt.plot(mean_fpr, mean_tpr, 'k--',
-             label='Mean ROC (AUC = ' + str(round(mean_auc,4)) + ')',lw=1)
+             label='Mean ROC (AUC = ' + str(round(mean_auc, 4)) + ')', lw=1)
 
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.0])
@@ -249,4 +260,3 @@ def crossValidation(trainingFile, n_splits = 10):
     plt.title('Receiver operating characteristic curves')
     plt.legend(loc="lower right")
     plt.show()
-
